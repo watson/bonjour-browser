8935147bc7d1e7c5a79f6478692ae57bb7855653
Jannis R <mail@jannisr.de>
2019-07-28 16:01:55 +0200

    CLI: add --help flag

--- index.js
+++ index.js
@@ -1,9 +1,25 @@
 #!/usr/bin/env node
 'use strict'
 
+var mri = require('mri')
+
+var argv = mri(process.argv.slice(2), {
+  boolean: ['help', 'h']
+})
+
+if (argv.help || argv.h) {
+  process.stdout.write(`\
+Usage:
+  bonjour-browser [name]
+Example:
+  bonjour-browser my-apple-tv
+`)
+  process.exit(0)
+}
+
 var bonjour = require('bonjour')()
 
-var name = process.argv[2]
+var name = argv._[2]
 var found = Object.create(null)
 
 bonjour.find({ type: name }, function (service) {
